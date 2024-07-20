498530b48b686933e22df0bfcb7acd8dd7208d4d
Kalila <69767640+digisomni@users.noreply.github.com>
2024-07-20 13:24:35 +0700

    Lint.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,30 +1,30 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
-import { MetaRequest, WorldTransport } from '../routes/router';
+import { MetaRequest, WorldTransport } from "../routes/router";
 
 const TEMP_PORT = 3000;
-const TEMP_ALLOWED_ORIGINS = '*';
-const TEMP_ALLOWED_METHODS_REQ = 'GET, POST, PUT, DELETE, OPTIONS';
-const TEMP_ALLOWED_HEADERS_REQ = 'Content-Type, Authorization';
-const TEMP_ALLOWED_METHODS_WT = 'GET, POST';
+const TEMP_ALLOWED_ORIGINS = "*";
+const TEMP_ALLOWED_METHODS_REQ = "GET, POST, PUT, DELETE, OPTIONS";
+const TEMP_ALLOWED_HEADERS_REQ = "Content-Type, Authorization";
+const TEMP_ALLOWED_METHODS_WT = "GET, POST";
 
 function init() {
     const expressApp = express();
 
     // Requests via Express
     expressApp.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', TEMP_ALLOWED_ORIGINS);
-        res.setHeader('Access-Control-Allow-Methods', TEMP_ALLOWED_METHODS_REQ);
-        res.setHeader('Access-Control-Allow-Headers', TEMP_ALLOWED_HEADERS_REQ);
-        if (req.method === 'OPTIONS') {
+        res.setHeader("Access-Control-Allow-Origin", TEMP_ALLOWED_ORIGINS);
+        res.setHeader("Access-Control-Allow-Methods", TEMP_ALLOWED_METHODS_REQ);
+        res.setHeader("Access-Control-Allow-Headers", TEMP_ALLOWED_HEADERS_REQ);
+        if (req.method === "OPTIONS") {
             return res.sendStatus(200);
         }
         return next();
     });
 
-    expressApp.use('/', MetaRequest);
+    expressApp.use("/", MetaRequest);
 
     // Create HTTP server
     const expressServer = createServer(expressApp);
