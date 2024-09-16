449fab9d4612c68aabffb60e65a84f4467b8ed56
Kalila <69767640+digisomni@users.noreply.github.com>
2024-09-16 18:26:15 +1000

    WIP adding tests.

--- src/client/deno/client.ts
+++ src/client/deno/client.ts
@@ -1,23 +1,24 @@
+/// <reference lib="deno.ns" />
+
 import { Agent } from './modules/vircadia-world-meta/client/modules/agent/agent.ts';
 import { Agent_Store } from './modules/vircadia-world-meta/client/modules/agent/agent_store.ts';
 import { Agent_World } from "./modules/vircadia-world-meta/client/modules/agent/agent_world.ts";
 import { log } from "./modules/vircadia-world-meta/general/modules/log.ts";
 
-export namespace Browser_Client {
+export namespace Deno_Client {
     export const worldConnected = () => Agent_Store.world !== null;
 
     export namespace Setup {
-        export const InitializeVircadiaWorld = (data: {
-            agentId: string;
+        export const initialize = (data: {
             iceServers?: RTCIceServer[];
         }) => {
-            log({ message: `Initializing Vircadia World for agent ${data.agentId}`, type: 'info' });
+            log({ message: `Initializing Vircadia World`, type: 'info' });
             Agent.initialize({ iceServers: data.iceServers });
             log({ message: 'Initialized Agent module', type: 'info' });
             log({ message: 'Vircadia World initialized', type: 'success' });
         };
 
-        export const ConnectToVircadiaWorld = (data: {
+        export const connect = (data: {
             agentId: string;
             capabilities: { useWebRTC: boolean };
             host: string;
@@ -27,11 +28,35 @@ export namespace Browser_Client {
             Agent_World.connectToWorld(data);
         };
     }
+
+    export namespace Test {
+        export const all = async () => {
+            log({ message: 'Testing all', type: 'info' });
+            Setup.initialize({});
+            Setup.connect({ 
+                agentId: '1234567890', 
+                capabilities: { useWebRTC: false }, 
+                host: 'localhost', 
+                port: 3000, 
+                key: '1234567890' 
+            });
+            
+            // Add a delay to ensure connection is established
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+            // Test database connection
+            await Test.databaseConnection();
+        }
+
+        export const databaseConnection = async () => {
+            log({ message: 'Testing database connection', type: 'info' });
+            await Agent_World.Test.databaseConnection();
+        }
+    }
 }
 
-
-    // await Client.Setup.InitializeVircadiaWorld({
-    //     host: "http://localhost",
-    //     port: 3000,
-    //     agentId: "1234567890",
-    // });
+if (import.meta.main) {
+    Deno.test("all", async () => {
+        await Deno_Client.Test.all();
+    });
+}
