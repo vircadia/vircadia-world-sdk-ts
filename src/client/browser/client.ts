ad5f891dfd250272d17267bd591620775d53a2ac
Kalila <69767640+digisomni@users.noreply.github.com>
2024-09-23 13:36:36 +1000

    Update.

--- src/client/browser/client.ts
+++ src/client/browser/client.ts
@@ -1,19 +1,5 @@
-import { Agent } from '../../shared/modules/vircadia-world-meta/client/modules/agent/agent.ts';
+import { Agent as Agent_Module } from '../../shared/modules/vircadia-world-meta/client/modules/agent/agent.ts';
 
 export namespace Browser_Client {
-    export const worldConnected = () => Agent.World.connected();
-
-    export namespace Setup {
-        export const InitializeVircadiaWorld = (data: {
-            agentId: string;
-        }) => {
-            // log(`Initializing Vircadia World for agent ${data.agentId}`, 'info');
-            // // Initialize Agent and Media modules
-            // Agent.initialize(data.agentId);
-            // log('Initialized Agent module', 'info');
-            // WebRTC_Media.InitializeMediaModule();
-            // log('Initialized Media module', 'info');
-            // log('Vircadia World initialized', 'success');
-        };
-    }
+    export const Agent = Agent_Module;
 }
