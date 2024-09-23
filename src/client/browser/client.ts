833754ffa610b32161b55c6f2d602dd10021880d
Kalila <69767640+digisomni@users.noreply.github.com>
2024-09-23 23:48:16 +1000

    WIP

--- src/client/browser/client.ts
+++ src/client/browser/client.ts
@@ -1,4 +1,4 @@
-import { Agent as Agent_Module } from '../../shared/modules/vircadia-world-meta/client/modules/agent/agent.ts';
+import { Agent as Agent_Module } from '../../shared/modules/vircadia-world-meta/client/modules/agent/index.ts';
 
 export namespace Browser_Client {
     export const Agent = Agent_Module;
