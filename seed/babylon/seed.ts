bbab799ce29a6eb1a33eaeeb6b02ce70db687a78
Kalila <69767640+digisomni@users.noreply.github.com>
2024-10-25 16:08:06 +1100

    Convert to bun instead of deno.

--- seed/babylon/seed.ts
+++ seed/babylon/seed.ts
@@ -1,12 +1,14 @@
-import { z } from 'zod'
+import { Engine } from "@babylonjs/core";
+import { z } from "zod";
 
 function seedScript(context: any): void {
-    const { scene, mesh, BABYLON } = context;
+	const { scene, mesh, BABYLON } = context;
 
-    console.info('Scene:', scene);
-    console.info('Mesh:', mesh);
-    console.info('BABYLON:', BABYLON);
-    console.info('zod:', z);
+	console.info("Scene:", scene);
+	console.info("Mesh:", mesh);
+	console.info("BABYLON:", BABYLON);
+	console.info("zod:", z);
+	console.info("Engine:", Engine);
 }
 
 seedScript({});
