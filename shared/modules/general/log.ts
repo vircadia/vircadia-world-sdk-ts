f8908615ca8fc3c29af67589e567d80c9fd92e33
Kalila <contact@vircadia.com>
2024-11-07 04:04:04 +0000

    WIP to update host and port handling.

--- shared/modules/general/log.ts
+++ shared/modules/general/log.ts
@@ -1,36 +1,57 @@
-import EC from 'eight-colors';
+import EC from "eight-colors";
 
 export function log(data: {
-    message: string,
-    type?: 'info' | 'success' | 'error' | 'warning' | 'warn' | 'debug',
-    debug?: boolean,
+	message: string;
+	type?: "info" | "success" | "error" | "warning" | "warn" | "debug";
+	debug?: boolean;
+	prefix?: string;
 }): void {
-    if (!data.type) {
-        console.log(data.message);
-        return;
-    }
+	if (!data.type) {
+		console.log(data.message);
+		return;
+	}
 
-    switch (data.type) {
-        case 'debug':
-            if (data.debug) {
-                console.debug(EC.blue(`ℹ ${data.message}`));
-            }
-            break;
-        case 'info':
-            console.info(EC.blue(`ℹ ${data.message}`));
-            break;
-        case 'success':
-            console.log(EC.green(`✔ ${data.message}`));
-            break;
-        case 'error':
-            console.error(EC.red(`✖ ${data.message}`));
-            break;
-        case 'warn':
-        case 'warning':
-            console.warn(EC.yellow(`⚠ ${data.message}`));
-            break;
-        default:
-            console.info(EC.white(`? ${data.message}`));
-            break;
-    }
+	switch (data.type) {
+		case "debug":
+			if (data.debug) {
+				console.debug(
+					EC.blue(
+						`${data.prefix ? `[${data.prefix}]: ` : ""} ℹ ${data.message}`,
+					),
+				);
+			}
+			break;
+		case "info":
+			console.info(
+				EC.blue(`${data.prefix ? `[${data.prefix}]: ` : ""} ℹ ${data.message}`),
+			);
+			break;
+		case "success":
+			console.log(
+				EC.green(
+					`${data.prefix ? `[${data.prefix}]: ` : ""} ✔ ${data.message}`,
+				),
+			);
+			break;
+		case "error":
+			console.error(
+				EC.red(`${data.prefix ? `[${data.prefix}]: ` : ""} ✖ ${data.message}`),
+			);
+			break;
+		case "warn":
+		case "warning":
+			console.warn(
+				EC.yellow(
+					`${data.prefix ? `[${data.prefix}]: ` : ""} ⚠ ${data.message}`,
+				),
+			);
+			break;
+		default:
+			console.info(
+				EC.white(
+					`${data.prefix ? `[${data.prefix}]: ` : ""} ? ${data.message}`,
+				),
+			);
+			break;
+	}
 }
