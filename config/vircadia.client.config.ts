73e05f8d6c197239402ecef56624e5877b9bf119
Kalila <69767640+digisomni@users.noreply.github.com>
2024-12-14 22:29:38 +1100

    Init revised schema + add postgrest (not done)

--- config/vircadia.client.config.ts
+++ config/vircadia.client.config.ts
@@ -1,33 +1,39 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-    VRCA_CLIENT_DEFAULT_TITLE: z.string().default("Vircadia World Web Interface"),
+    VRCA_CLIENT_DEFAULT_TITLE: z
+        .string()
+        .default("Vircadia World Web Interface"),
     VRCA_CLIENT_DEFAULT_DESCRIPTION: z.string().default("..."),
-    VRCA_CLIENT_DEFAULT_URL: z.string().url().default("https://app.vircadia.com"),
+    VRCA_CLIENT_DEFAULT_URL: z
+        .string()
+        .url()
+        .default("https://app.vircadia.com"),
     VRCA_CLIENT_DEFAULT_OG_IMAGE: z.string().default("/brand/logo_icon.webp"),
     VRCA_CLIENT_DEFAULT_OG_TYPE: z.string().default("website"),
     VRCA_CLIENT_DEFAULT_FAVICON: z.string().default("/brand/favicon.svg"),
 
-    VRCA_CLIENT_BASE_APP_URL: z.string().url().default("https://app.vircadia.com"),
-
-    VRCA_CLIENT_DEFAULT_WORLD_SUPABASE_URL: z
+    VRCA_CLIENT_BASE_APP_URL: z
         .string()
         .url()
-        .default("https://api-antares.vircadia.com"),
-    VRCA_CLIENT_DEFAULT_WORLD_SUPABASE_ANON_KEY: z
+        .default("https://app.vircadia.com"),
+
+    VRCA_CLIENT_DEFAULT_WORLD_SERVER_URL: z
         .string()
-        .default(
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
-        ),
+        .url()
+        .default("localhost:8000"),
+    VRCA_CLIENT_DEFAULT_WORLD_DATABASE_URL: z
+        .string()
+        .default("localhost:5432"),
 
     VRCA_CLIENT_DEFAULT_WORLD_ACCOUNT_USERNAME: z
         .string()
         .nullable()
-        .default("admin@changeme.com"),
+        .default(null),
     VRCA_CLIENT_DEFAULT_WORLD_ACCOUNT_PASSWORD: z
         .string()
         .nullable()
-        .default("CHANGE_ME!"),
+        .default(null),
 });
 
 const env = envSchema.parse(import.meta.env);
@@ -40,8 +46,8 @@ export const VircadiaConfig_Client = {
     defaultOgImage: env.VRCA_CLIENT_DEFAULT_OG_IMAGE,
     defaultOgType: env.VRCA_CLIENT_DEFAULT_OG_TYPE,
     baseAppUrl: env.VRCA_CLIENT_BASE_APP_URL,
-    defaultWorldSupabaseUrl: env.VRCA_CLIENT_DEFAULT_WORLD_SUPABASE_URL,
-    defaultWorldSupabaseAnonKey: env.VRCA_CLIENT_DEFAULT_WORLD_SUPABASE_ANON_KEY,
+    defaultWorldServerUrl: env.VRCA_CLIENT_DEFAULT_WORLD_SERVER_URL,
+    defaultWorldDatabaseUrl: env.VRCA_CLIENT_DEFAULT_WORLD_DATABASE_URL,
     defaultWorldAccountUsername: env.VRCA_CLIENT_DEFAULT_WORLD_ACCOUNT_USERNAME,
     defaultWorldAccountPassword: env.VRCA_CLIENT_DEFAULT_WORLD_ACCOUNT_PASSWORD,
 };
