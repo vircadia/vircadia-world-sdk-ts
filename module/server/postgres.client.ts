87d01a81bf9f7d0e6c2d032aa95e3ff0feb37100
Kalila <69767640+digisomni@users.noreply.github.com>
2025-03-03 16:37:51 +0800

    WIP

--- module/server/postgres.client.ts
+++ module/server/postgres.client.ts
@@ -25,15 +25,15 @@ export class PostgresClient {
                     VircadiaConfig.SERVER.SERVICE.POSTGRES.HOST_EXTERNAL,
                 PORT_EXTERNAL:
                     VircadiaConfig.SERVER.SERVICE.POSTGRES.PORT_EXTERNAL,
-                host_cluster:
+                HOST_CLUSTER:
                     VircadiaConfig.SERVER.SERVICE.POSTGRES.HOST_CLUSTER,
-                port_cluster:
+                PORT_CLUSTER:
                     VircadiaConfig.SERVER.SERVICE.POSTGRES.PORT_CLUSTER,
-                database: VircadiaConfig.SERVER.SERVICE.POSTGRES.DATABASE,
-                superUser: VircadiaConfig.GLOBAL_CONSTS.DB_SUPER_USER,
-                agentProxyUser:
+                DATABASE: VircadiaConfig.SERVER.SERVICE.POSTGRES.DATABASE,
+                SUPERUSER: VircadiaConfig.GLOBAL_CONSTS.DB_SUPER_USER,
+                AGENT_PROXY_USER:
                     VircadiaConfig.GLOBAL_CONSTS.DB_AGENT_PROXY_USER,
-                agentProxyPassword:
+                AGENT_PROXY_PASSWORD:
                     VircadiaConfig.SERVER.SERVICE.POSTGRES.AGENT_PROXY_PASSWORD,
             },
         });
