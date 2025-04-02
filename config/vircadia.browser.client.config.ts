6fb0d017cca89e508300ac7b818be141f8aef645
Kalila <69767640+digisomni@users.noreply.github.com>
2025-04-02 12:18:57 +0800

    WIP to remove dev client container.

--- config/vircadia.browser.client.config.ts
+++ config/vircadia.browser.client.config.ts
@@ -37,41 +37,14 @@ const browserClientEnvSchema = z.object({
     VRCA_CLIENT_WEB_BABYLON_JS_PRODUCTION_PORT_CONTAINER_BIND_INTERNAL: z.coerce
         .number()
         .default(3025),
-    VRCA_CLIENT_WEB_BABYLON_JS_PRODUCTION_IS_ENABLED: z
-        .union([
-            z.boolean(),
-            z
-                .string()
-                .transform(
-                    (val) => val === "1" || val.toLowerCase() === "true",
-                ),
-        ])
-        .default(false),
 
     // Development Web Babylon JS Client
     VRCA_CLIENT_WEB_BABYLON_JS_DEV_CONTAINER_NAME: z
         .string()
         .default("vircadia_world_client_web_babylon_js_dev"),
-    VRCA_CLIENT_WEB_BABYLON_JS_DEV_HOST_CONTAINER_BIND_EXTERNAL: z
-        .string()
-        .default("0.0.0.0"),
-    VRCA_CLIENT_WEB_BABYLON_JS_DEV_PORT_CONTAINER_BIND_INTERNAL: z.coerce
-        .number()
-        .default(3066),
-    VRCA_CLIENT_WEB_BABYLON_JS_DEV_PORT_CONTAINER_BIND_EXTERNAL: z.coerce
-        .number()
-        .default(3066),
+    VRCA_CLIENT_WEB_BABYLON_JS_DEV_HOST: z.string().default("0.0.0.0"),
+    VRCA_CLIENT_WEB_BABYLON_JS_DEV_PORT: z.coerce.number().default(3066),
     VRCA_CLIENT_WEB_BABYLON_JS_DEBUG_SESSION_TOKEN: z.string().default(""),
-    VRCA_CLIENT_WEB_BABYLON_JS_DEV_IS_ENABLED: z
-        .union([
-            z.boolean(),
-            z
-                .string()
-                .transform(
-                    (val) => val === "1" || val.toLowerCase() === "true",
-                ),
-        ])
-        .default(false),
 
     VRCA_CLIENT_WEB_BABYLON_JS_META_TITLE_BASE: z.string().default("Vircadia"),
     VRCA_CLIENT_WEB_BABYLON_JS_META_DESCRIPTION: z.string().default("..."),
