2fd3b3a93e64ed68644bd0ef2cc6d0f7ea914305
revofire <69767640+digisomni@users.noreply.github.com>
2025-08-17 20:16:49 +0800

    Provider improvements [pre migrate to SDK]

--- browser/src/index.browser.vue.ts
+++ browser/src/index.browser.vue.ts
@@ -4,3 +4,9 @@ export * from "./vue/provider/useVircadia";
 
 // Schema exports
 export * from "../../schema/src/vircadia.schema.general";
+
+// Core exports for direct client usage in Vue apps
+export {
+    ClientCore,
+    type ClientCoreConnectionInfo,
+} from "./core/vircadia.client.browser.core";
